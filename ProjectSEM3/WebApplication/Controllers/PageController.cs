e9885014533a2785194fbb30156ef10d98fce2b3
dungki <67158859+dungki@users.noreply.github.com>
2021-02-16 08:16:33 +0700

    Update HTML

---
+++ ProjectSEM3/WebApplication/Controllers/PageController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class PageController : Controller
+    {
+        // GET: Page
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Job_list()
+        {
+            return View();
+        }
+        public ActionResult Job_single()
+        {
+            return View();
+        }
+    }
+}
