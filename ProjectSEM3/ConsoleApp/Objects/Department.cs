e3b0c5bba3cc07b41ab9463651f11e6a8348d694
dungki <67158859+dungki@users.noreply.github.com>
2021-02-17 14:09:51 +0700

    Second Update

---
+++ ProjectSEM3/ConsoleApp/Objects/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Objects
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Create_At { get; set; }
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
---
+++ ProjectSEM3/ConsoleApp/Objects/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Objects
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Email { get; set; }
+        public string Avatar { get; set; }
+        public int DepartmentId { get; set; }
+        [ForeignKey("DepartmentId")]
+        public Department Department { get; set; }
+    }
+}
